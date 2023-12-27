05d950609c5c8ab03b77d5f73a97e28e1ffee78f
Acnivator <96819718+Acnivator@users.noreply.github.com>
2023-12-27 10:59:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,78 +1,42 @@
 import socket
-from threading import Thread
+import threading
 
-# Server configuration
-IP_ADDRESS = "127.0.0.1"
-PORT = 12345
+# Ask the user to choose their nickname
+nickname = input("Enter your nickname: ")
 
-# List to maintain connected clients and their nicknames
-clients = []
-nicknames = []
+# Create a client with AF_INET and SOCK_STREAM
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Define the IP Address and Port number
+IP = '127.0.0.1'
+PORT = 1234
 
-# Bind the server to the IP and Port
-server_socket.bind((IP_ADDRESS, PORT))
+# Connect the client to the server
+client.connect((IP, PORT))
 
-# Listen for incoming connections
-server_socket.listen(5)
-print(f"Server listening on {IP_ADDRESS}:{PORT}")
-
-def clientthread(client_socket, nickname):
-    global clients, nicknames
-
-    # Send a welcome message to the client
-    client_socket.send("Welcome to the Quiz Game!".encode('utf-8'))
-
+# Function to receive messages from the server
+def receive():
     while True:
         try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                # If the message is empty, the client has disconnected
-                remove_client(client_socket, nickname)
-                break
-            # Broadcast the message to all clients
-            broadcast(f"{nickname}: {message}", client_socket)
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            remove_client(client_socket, nickname)
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
             break
 
-def remove_client(client_socket, nickname):
-    global clients, nicknames
+# Function to send messages to the server
+def write():
+    while True:
+        message = input()
+        client.send(message.encode('utf-8'))
 
-    # Remove the client and their nickname from the lists
-    clients.remove(client_socket)
-    nicknames.remove(nickname)
-    print(f"Client {nickname} disconnected.")
+# Start the receive() and write() functions with threads
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
 
-def broadcast(message, sender_socket):
-    global clients
-
-    # Send the message to all clients except the sender
-    for client in clients:
-        if client != sender_socket:
-            try:
-                client.send(message.encode('utf-8'))
-            except Exception as e:
-                print(f"An error occurred while broadcasting: {e}")
-                remove_client(client, nicknames[clients.index(client)])
-
-while True:
-    # Accept incoming connection
-    client_socket, addr = server_socket.accept()
-
-    # Receive the nickname from the client
-    nickname = client_socket.recv(1024).decode('utf-8')
-    nicknames.append(nickname)
-
-    # Add the client to the list
-    clients.append(client_socket)
-
-    # Send a welcome message to the client
-    client_socket.send("Welcome to the Quiz Game!".encode('utf-8'))
-
-    # Create a new thread for the client
-    thread = Thread(target=clientthread, args=(client_socket, nickname))
-    thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
